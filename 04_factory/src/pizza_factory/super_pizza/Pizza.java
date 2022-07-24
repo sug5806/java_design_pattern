ca74be030880ee0e371920e38af1b715c3d8567e
홍동수 <ds.h@purple.io>
2022-07-24 12:06:16 +0900

    04.팩토리패턴
    추상 클래스 피자 변경

--- 04_factory/src/pizza_factory/super_pizza/Pizza.java
+++ 04_factory/src/pizza_factory/super_pizza/Pizza.java
@@ -1,23 +1,18 @@
 package pizza_factory.super_pizza;
 
-import java.util.ArrayList;
-import java.util.List;
+import pizza_factory.ingredient.ingredient_interface.*;
 
 public abstract class Pizza {
-    protected String name;
-    protected String dough;
-    protected String sauce;
-    protected List<String> toppings = new ArrayList<>();
+    String name;
 
-    public void prepare() {
-        System.out.println("준비중: " + name);
-        System.out.println("도우를 돌리는 중...");
-        System.out.println("소스를 뿌리는 중...");
-        System.out.println("토핑을 올리는 중: ");
-        for (String topping : toppings) {
-            System.out.println("   " + topping);
-        }
-    }
+    Dough dough;
+    Sauce sauce;
+    Veggies veggies[];
+    Cheese cheese;
+    Pepperoni pepperoni;
+    Clams clams;
+
+    abstract void prepare();
 
     public void bake() {
         System.out.println("175도에서 25분 간 굽기");
@@ -35,14 +30,13 @@ public abstract class Pizza {
         return name + "\n";
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String toString() {
         StringBuffer display = new StringBuffer();
         display.append("---- " + name + " ----\n");
-        display.append(dough + "\n");
-        display.append(sauce + "\n");
-        for (String topping : toppings) {
-            display.append(topping + "\n");
-        }
         return display.toString();
     }
 }
