ec11bdbf6391d29da84756b163850d1368484ad6
홍동수 <ds.h@purple.io>
2022-07-31 11:05:03 +0900

    06_command_pattern
    조명 off 추가

---
+++ 06_command/src/simple_remote_control/light/LightOffCommand.java
@@ -0,0 +1,16 @@
+package simple_remote_control.light;
+
+import simple_remote_control.command.Command;
+
+public class LightOffCommand implements Command {
+    Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.off();
+    }
+}
