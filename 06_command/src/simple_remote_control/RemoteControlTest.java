dfc49adcf1302f46d507ba07989049541d8b71db
홍동수 <ds.h@purple.io>
2022-07-31 10:40:12 +0900

    06_command_pattern
    주석 추가

--- 06_command/src/simple_remote_control/RemoteControlTest.java
+++ 06_command/src/simple_remote_control/RemoteControlTest.java
@@ -1,11 +1,18 @@
 package simple_remote_control;
 
+// 커맨드 패턴 클라이언트
 public class RemoteControlTest {
     public static void main(String[] args) {
+        // 인보커 역할을 하여 필요한 작업을 요청할때 사용할 커맨드 객체를 인자로 전달받음
         SimpleRemoteControl simpleRemoteControl = new SimpleRemoteControl();
 
-        LightOnCommand lightOnCommand = new LightOnCommand(new Light());
+        // 요청을 받아 처리할 리시버
+        Light light = new Light();
 
+        // 커맨드 객체를 생성하여 리시버 전달
+        LightOnCommand lightOnCommand = new LightOnCommand(light);
+
+        // 커맨드 객체를 인보커에게 전달
         simpleRemoteControl.setSlot(lightOnCommand);
         simpleRemoteControl.buttonWasPressed();
     }
