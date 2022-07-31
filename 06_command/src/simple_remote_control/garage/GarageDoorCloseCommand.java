9949c951f425f17bea1e05f0ccfc2a9de07236dc
홍동수 <ds.h@purple.io>
2022-07-31 11:13:57 +0900

    06_command_pattern
    스테레오 오디오, 차고 닫기 Command 추가

---
+++ 06_command/src/simple_remote_control/garage/GarageDoorCloseCommand.java
@@ -0,0 +1,16 @@
+package simple_remote_control.garage;
+
+import simple_remote_control.command.Command;
+
+public class GarageDoorCloseCommand implements Command {
+    GarageDoor garageDoor;
+
+    public GarageDoorCloseCommand(GarageDoor garageDoor) {
+        this.garageDoor = garageDoor;
+    }
+
+    @Override
+    public void execute() {
+        garageDoor.down();
+    }
+}
---
+++ 06_command/src/simple_remote_control/stereoAudio/StereoOffCommand.java
@@ -0,0 +1,16 @@
+package simple_remote_control.stereoAudio;
+
+import simple_remote_control.command.Command;
+
+public class StereoOffCommand implements Command {
+    StereoAudio stereoAudio;
+
+    public StereoOffCommand(StereoAudio stereoAudio) {
+        this.stereoAudio = stereoAudio;
+    }
+
+    @Override
+    public void execute() {
+        stereoAudio.off();
+    }
+}
